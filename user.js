cfb57d679e97733ecf18a799bf6fdff273b6313a
athiraanil11 <athiraanil093@gmail.com>
2022-06-26 20:13:06 +0530

    work completed

---
+++ user.js
@@ -0,0 +1,58 @@
+loginBtnEvent=()=>
+{
+
+    var username=document.getElementById("username").value 
+    var password=document.getElementById("password").value
+
+
+
+    {
+        console.log(username)
+        alert(username)
+        console.log(password)
+        alert(password)
+       
+    }
+    
+
+}
+regBtnEvent=()=>
+{
+    var name=document.getElementById("name").value 
+    var address=document.getElementById("address").value
+    var mobile=document.getElementById("mobile").value 
+    var emailid=document.getElementById("emailid").value
+    var bloodgroup=document.getElementById("bloodgroup").value 
+    var college=document.getElementById("college").value
+    var username=document.getElementById("username").value 
+    var password=document.getElementById("password").value
+    var confirmpassword=document.getElementById("confirmpassword").value
+
+
+
+   
+
+{
+   
+    console.log(name)
+    alert(name)
+    console.log(address)
+    alert(address)
+    console.log(mobile)
+    alert(mobile)
+    console.log(emailid)
+    alert(emailid)
+    console.log(bloodgroup)
+    alert(bloodgroup)
+    console.log(college)
+    alert(college)
+    console.log(username)
+    alert(username)
+    console.log(password)
+    alert(password)
+   console.log(confirmpassword)
+   alert(confirmpassword)
+    
+    
+}
+}
